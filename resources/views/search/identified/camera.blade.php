2f08d8d1764daceaba44711ef7de5671c897176b
Minh-Triet <74360861+Minh-Triet@users.noreply.github.com>
2022-04-13 15:12:09 +0700

    camera_new

--- resources/views/search/identified/camera.blade.php
+++ resources/views/search/identified/camera.blade.php
@@ -1,62 +1,41 @@
-{{--<!DOCTYPE html>--}}
-{{--<html lang=en>--}}
-{{--<head>--}}
-{{--    <meta charset=UTF-8>--}}
-{{--    <meta name=viewport content="width=device-width,initial-scale=1">--}}
-{{--    <meta http-equiv=X-UA-Compatible content="ie=edge">--}}
-{{--    <title>Cách sử dụng JavaScript để truy cập camera của thiết bị (trước và sau)</title>--}}
 @extends('layouts.tri')
-
 @section('body')
-@yield('style')
-{{--</head>--}}
-{{--<body>--}}
+    <!-- Styles and Fonts -->
+    <link rel="stylesheet" href="{{ versioned_asset('dist/styles.css') }}">
+    <link rel="stylesheet" media="print" href="{{ versioned_asset('dist/print-styles.css') }}">
+    @yield('style')
+    <main class="content-wrap mt-m card">
 
-<main class="content-wrap mt-m card">
-<h1 class="list-heading">Identify cooking ingredients</h1><br>
-{{--<a href=https://anonystick.com/blog-developer/su-dung-javascript-truy-cap-camera-sau-truoc-va-chup-man-minh-thiet-bi-di-dong-2020061162339379>&#8592;--}}
-{{--    Trở về với bài viết</a><br>--}}
-{{--<div class=btns>--}}
-{{--    <button class="button is-hidden" id=btnPlay>Play camera</button>--}}
-{{--    <button class=button id=btnPause>Stop camera</button>--}}
-{{--    <button class=button id=btnScreenshot>Chụp hình</button>--}}
-{{--    <button class=button id=btnChangeCamera style="padding: 6px 10px;">Đổi camera</button>--}}
-{{--</div>--}}
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+            <form method="post" enctype="multipart/form-data" >
+                <div>
+                    <label for="image_uploads">Choose images to search (PNG, JPG)</label>
+                    <input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" multiple>
+                </div>
+                <div class="preview">
+                    <p>No files currently selected for upload</p>
+                </div>
+            </form>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    ch1>Demo file upload to Deep Learning API</hl>
-    <input id="sortpicture" type="file" name="sortpic" />
-    <button id="upload">Upload</button>
+            <button id="upload" class="button">Search</button>
 
-
-
-
-
-</main>
+    </main>
 @stop
 
 @section('left')
     @include('home.parts.sidebar')
 @stop
 
-@section('right')
-    <div class="actions mb-xl">
-        <h5>{{ trans('common.actions') }}</h5>
-        <div class="icon-list text-primary">
-            <a class="icon-list-item is-hidden" id=btnPlay>Play Camera</a>
-            <a class=icon-list-item id=btnPause>Stop Camera</a>
-            <a class=icon-list-item id=btnScreenshot>Take a Photo</a>
-            <a class=icon-list-item id=btnChangeCamera>Change Camera</a>
-{{--            @if(user()->can('recipemenu-create-all'))--}}
-{{--                <a href="{{ url("/create-menu") }}" class="icon-list-item">--}}
-{{--                    <span>@icon('add')</span>--}}
-{{--                    <span>{{ trans('entities.menus_new_action') }}</span>--}}
-{{--                </a>--}}
-{{--            @endif--}}
-{{--            @include('entities.view-toggle', ['view' => $view, 'type' => 'menus'])--}}
-{{--            @include('home.parts.expand-toggle', ['classes' => 'text-primary', 'target' => '.entity-list.compact .entity-item-snippet', 'key' => 'home-details'])--}}
-{{--            @include('common.dark-mode-toggle', ['classes' => 'icon-list-item text-primary'])--}}
-        </div>
-    </div>
-@stop
+@yield('bottom')
+
+
+
+
+
+
+
+
+
+
+
 
--- resources/views/search/identified/webcam.blade.php
+++ resources/views/search/identified/webcam.blade.php
@@ -1,9 +1,8 @@
 @extends('search.identified.camera')
 @section('scripts')
-
         <script>
             $('#upload').on('click', function() {
-            var file_data = $('#sortpicture').prop('files')[0];
+            var file_data = $('#image_uploads').prop('files')[0];
             var form_data = new FormData();
             form_data.append('file', file_data);
             //alert(form_data);
@@ -17,14 +16,151 @@
             type: 'post',
             success: function(api_resp){
                     alert('Tấn ảnh nay là :'+ api_resp); // display response from the PHP script, if any
+
             },
             error: function(err) {
                 alert(err.responseText);
             }
         });
             });
-    </script>
-@stop
-@section('style')
 
+                const input = document.querySelector('input');
+                const preview = document.querySelector('.preview');
+
+                input.style.opacity = 0;
+
+                input.addEventListener('change', updateImageDisplay);
+
+                function updateImageDisplay() {
+                while(preview.firstChild) {
+                preview.removeChild(preview.firstChild);
+            }
+
+                const curFiles = input.files;
+                if(curFiles.length === 0) {
+                const para = document.createElement('p');
+                para.textContent = 'No files currently selected for upload';
+                preview.appendChild(para);
+            } else {
+                const list = document.createElement('ol');
+                preview.appendChild(list);
+
+                for(const file of curFiles) {
+                const listItem = document.createElement('li');
+                const para = document.createElement('p');
+
+                if(validFileType(file)) {
+                para.textContent = `File name ${file.name}, file size ${returnFileSize(file.size)}.`;
+                const image = document.createElement('img');
+                image.src = URL.createObjectURL(file);
+
+                listItem.appendChild(image);
+                listItem.appendChild(para);
+            } else {
+                para.textContent = `File name ${file.name}: Not a valid file type. Update your selection.`;
+                listItem.appendChild(para);
+            }
+
+                list.appendChild(listItem);
+            }
+            }
+            }
+
+                // https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types
+                const fileTypes = [
+                'image/apng',
+                'image/bmp',
+                'image/gif',
+                'image/jpeg',
+                'image/pjpeg',
+                'image/png',
+                'image/svg+xml',
+                'image/tiff',
+                'image/webp',
+                `image/x-icon`
+                ];
+
+                function validFileType(file) {
+                return fileTypes.includes(file.type);
+            }
+
+                function returnFileSize(number) {
+                if(number < 1024) {
+                return number + 'bytes';
+            } else if(number > 1024 && number < 1048576) {
+                return (number/1024).toFixed(1) + 'KB';
+            } else if(number > 1048576) {
+                return (number/1048576).toFixed(1) + 'MB';
+            }
+            }
+        </script>
 @stop
+
+@section('style')
+    <style>
+        html {
+            font-family: sans-serif;
+        }
+
+        form ol {
+            padding-left: 0;
+        }
+
+        form li, div > p {
+            background: #eee;
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 10px;
+            list-style-type: none;
+            border: 1px solid black;
+        }
+
+        form img {
+            height: 64px;
+            order: 1;
+        }
+
+        form p {
+            line-height: 32px;
+            padding-left: 10px;
+        }
+
+        form label, form button {
+            background-color: #7F9CCB;
+            padding: 5px 10px;
+            border-radius: 5px;
+            border: 1px ridge black;
+            font-size: 0.8rem;
+            height: auto;
+        }
+
+        form label:hover, form button:hover {
+            background-color: #2D5BA3;
+            color: white;
+        }
+
+        form label:active, form button:active {
+            background-color: #0D3F8F;
+            color: white;
+        }
+        .button {
+            background-color: #4CAF50; /* Green */
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            border-radius: 5px;
+        }
+    </style>
+@stop
+@yield('bottom')
+
+@yield('scripts')
