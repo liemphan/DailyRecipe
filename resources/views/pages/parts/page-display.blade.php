e37b060304f354d57cd5a517f0a45359c921d5dd
Minh-Triet <74360861+Minh-Triet@users.noreply.github.com>
2022-04-23 15:38:44 +0700

    style

--- resources/views/pages/parts/page-display.blade.php
+++ resources/views/pages/parts/page-display.blade.php
@@ -1,7 +1,7 @@
-<div dir="auto">
+<div dir="auto" style="text-align:justify">
 
     <h1 class="break-text" id="bkmrk-page-title"><b>{{$recipe->name}}</b></h1>
-    <h5 class="text-muted"><i>{!! nl2br(e($recipe->description)) !!}</i></h5>
+    <h5 class="text-muted " ><i>{!! nl2br(e($recipe->description)) !!}</i></h5>
     <div style="clear:left;"></div>
 
     @if (isset($diff) && $diff)
